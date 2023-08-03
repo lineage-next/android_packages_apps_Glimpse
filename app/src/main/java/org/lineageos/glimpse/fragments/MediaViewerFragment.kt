6f53a7b44208a7eb9b2a985037a7829d1d3e38f7
Luca Stefani <luca.stefani.ge1@gmail.com>
2023-08-03 21:29:35 +0200

    Glimpse: Don't bother loading media from MediaStore in MediaViewer
    We receive all the data we care about via fragment arguments

--- app/src/main/java/org/lineageos/glimpse/fragments/MediaViewerFragment.kt
+++ app/src/main/java/org/lineageos/glimpse/fragments/MediaViewerFragment.kt
@@ -5,26 +5,19 @@
 
 package org.lineageos.glimpse.fragments
 
-import android.database.Cursor
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
-import androidx.loader.app.LoaderManager
-import androidx.loader.content.CursorLoader
-import androidx.loader.content.Loader
 import androidx.media3.common.C.TIME_UNSET
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
@@ -37,19 +30,14 @@ import org.lineageos.glimpse.models.Album
 import org.lineageos.glimpse.models.Media
 import org.lineageos.glimpse.models.MediaType
 import org.lineageos.glimpse.utils.CommonNavigationArguments
-import org.lineageos.glimpse.utils.MediaStoreRequests
-import org.lineageos.glimpse.utils.PermissionsUtils
 import java.text.SimpleDateFormat
-import java.util.Date
 
 /**
  * A fragment showing a media that supports scrolling before and after it.
  * Use the [MediaViewerFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MediaViewerFragment : Fragment(
-    R.layout.fragment_media_viewer
-), LoaderManager.LoaderCallbacks<Cursor> {
+class MediaViewerFragment : Fragment(R.layout.fragment_media_viewer) {
     // Views
     private val adjustButton by getViewProperty<ImageButton>(R.id.adjustButton)
     private val backButton by getViewProperty<ImageButton>(R.id.backButton)
@@ -73,35 +61,10 @@ class MediaViewerFragment : Fragment(
         }
     }
 
-    // Permissions
-    private val permissionsUtils by lazy { PermissionsUtils(requireContext()) }
-    private val mainPermissionsRequestLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) {
-        if (it.isNotEmpty()) {
-            if (!permissionsUtils.mainPermissionsGranted()) {
-                Toast.makeText(
-                    requireContext(), "No main permissions", Toast.LENGTH_SHORT
-                ).show()
-                requireActivity().finish()
-            } else {
-                initCursorLoader()
-            }
-        }
-    }
-
-    // MediaStore
-    private val loaderManagerInstance by lazy { LoaderManager.getInstance(this) }
-
     private var album: Album? = null
     private lateinit var media: Media
 
-    private var cursor: Cursor? = null
     private var position: Int = -1
-
-    private var idIndex = -1
-    private var mediaTypeIndex = -1
-    private var dateAddedIndex = -1
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -133,11 +96,7 @@ class MediaViewerFragment : Fragment(
             windowInsets
         }
 
-        if (!permissionsUtils.mainPermissionsGranted()) {
-            mainPermissionsRequestLauncher.launch(PermissionsUtils.mainPermissions)
-        } else {
-            initCursorLoader()
-        }
+        loadMedia()
     }
 
     override fun onDestroyView() {
@@ -148,106 +107,26 @@ class MediaViewerFragment : Fragment(
         exoPlayer.release()
     }
 
-    override fun onCreateLoader(id: Int, args: Bundle?) = when (id) {
-        MediaStoreRequests.MEDIA_STORE_REELS_LOADER_ID.ordinal -> {
-            val projection = arrayOf(
-                MediaStore.Files.FileColumns._ID,
-                MediaStore.Files.FileColumns.DATE_ADDED,
-                MediaStore.Files.FileColumns.MEDIA_TYPE,
-            )
-            val selection = buildString {
-                append("(")
-                append(buildString {
-                    append(MediaStore.Files.FileColumns.MEDIA_TYPE)
-                    append("=")
-                    append(MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE)
-                    append(" OR ")
-                    append(MediaStore.Files.FileColumns.MEDIA_TYPE)
-                    append("=")
-                    append(MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO)
-                })
-                append(")")
-                album?.let {
-                    append(
-                        buildString {
-                            append(" AND ")
-                            append(MediaStore.Files.FileColumns.BUCKET_ID)
-                            append(" = ?")
-                        }
-                    )
-                }
-            }
-            CursorLoader(
-                requireContext(),
-                MediaStore.Files.getContentUri("external"),
-                projection,
-                selection,
-                album?.let { arrayOf(it.id.toString()) },
-                MediaStore.Files.FileColumns.DATE_ADDED + " DESC"
-            )
-        }
-
-        else -> throw Exception("Unknown ID $id")
-    }
-
-    override fun onLoaderReset(loader: Loader<Cursor>) {
-        cursor = null
-
-        idIndex = -1
-        mediaTypeIndex = -1
-        dateAddedIndex = -1
-
-        loadMedia()
-    }
-
-    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
-        cursor = data
-
-        cursor?.let {
-            idIndex = it.getColumnIndex(MediaStore.Files.FileColumns._ID)
-            mediaTypeIndex = it.getColumnIndex(MediaStore.Files.FileColumns.MEDIA_TYPE)
-            dateAddedIndex = it.getColumnIndex(MediaStore.Files.FileColumns.DATE_ADDED)
-        }
-
-        loadMedia()
-    }
-
-    private fun initCursorLoader() {
-        loaderManagerInstance.initLoader(
-            MediaStoreRequests.MEDIA_STORE_REELS_LOADER_ID.ordinal, null, this
-        )
-    }
-
     private fun loadMedia() {
-        cursor?.also { cursor ->
-            cursor.moveToPosition(position)
-
-            media = Media(
-                cursor.getLong(idIndex),
-                MediaType.fromMediaStoreValue(cursor.getInt(mediaTypeIndex)),
-                Date(cursor.getLong(dateAddedIndex) * 1000),
-            )
-
-            when (media.mediaType) {
-                MediaType.IMAGE -> {
-                    imageView.load(media.externalContentUri)
-                }
-
-                MediaType.VIDEO -> {
-                    with(exoPlayer) {
-                        setMediaItem(MediaItem.fromUri(media.externalContentUri))
-                        seekTo(TIME_UNSET)
-                        prepare()
-                    }
-                }
+        when (media.mediaType) {
+            MediaType.IMAGE -> {
+                imageView.load(media.externalContentUri)
             }
 
-            dateTextView.text = dateFormatter.format(media.dateAdded)
-            timeTextView.text = timeFormatter.format(media.dateAdded)
-
-            imageView.isVisible = media.mediaType == MediaType.IMAGE
-            playerView.isVisible = media.mediaType == MediaType.VIDEO
+            MediaType.VIDEO -> {
+                with(exoPlayer) {
+                    setMediaItem(MediaItem.fromUri(media.externalContentUri))
+                    seekTo(TIME_UNSET)
+                    prepare()
+                }
+            }
         }
+
+        dateTextView.text = dateFormatter.format(media.dateAdded)
+        timeTextView.text = timeFormatter.format(media.dateAdded)
+
+        imageView.isVisible = media.mediaType == MediaType.IMAGE
+        playerView.isVisible = media.mediaType == MediaType.VIDEO
     }
 
     companion object {
