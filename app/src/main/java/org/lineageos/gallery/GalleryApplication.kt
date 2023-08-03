4c1e43746f3c1fc4b8eb6a492c768c1e20605d91
Luca Stefani <luca.stefani.ge1@gmail.com>
2023-08-03 18:25:15 +0200

    Gallery: Allow thumbails of Images/Videos/GIFs
    Change-Id: Ib038a717a990fb1f03427ebd5753f54fb531342b

--- app/src/main/java/org/lineageos/gallery/GalleryApplication.kt
+++ app/src/main/java/org/lineageos/gallery/GalleryApplication.kt
@@ -6,13 +6,24 @@
 package org.lineageos.gallery
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.decode.VideoFrameDecoder
 import com.google.android.material.color.DynamicColors
 
-class GalleryApplication : Application() {
+class GalleryApplication : Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
 
         // Observe dynamic colors changes
         DynamicColors.applyToActivitiesIfAvailable(this)
     }
+
+    override fun newImageLoader() = ImageLoader.Builder(this).components {
+        add(ImageDecoderDecoder.Factory())
+        add(GifDecoder.Factory())
+        add(VideoFrameDecoder.Factory())
+    }.build()
 }
